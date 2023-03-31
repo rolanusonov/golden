7ce49b61f85b018bacaa8a77d63dc4ae07780d22
rolanusonov <rolanusonov031209@gmail.com>
2023-03-31 15:40:29 +0900

    init

--- img/script.js
+++ img/script.js
@@ -35,3 +35,24 @@ DG.then(function () {
 
     DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
 });
+
+
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.nav');
+let menuItem = document.querySelectorAll('.nav__link');
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+
+
+menuItem.forEach (function(menuItem) {
+    menuItem.addEventListener('click',function(){
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
+})
+
+
